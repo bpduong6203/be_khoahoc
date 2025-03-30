91fca87ad57a7705e72827c35ac1735909880a72
Bui Duong <>
2025-03-30 08:26:20 +0700

    delete fb controller

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,6 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanc
 // login mạng xã hội
 Route::get('/auth/{provider}', [GoogleController::class, 'redirectToProvider']);
 Route::get('/auth/{provider}/callback', [GoogleController::class, 'handleProviderCallback']);
-Route::get('/auth/facebook', [FacebookController::class, 'redirectToProvider']);
-Route::get('/auth/facebook/callback', [FacebookController::class, 'handleProviderCallback']);
 
 // lấy thông tin người dùng
 Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'getUser']);
