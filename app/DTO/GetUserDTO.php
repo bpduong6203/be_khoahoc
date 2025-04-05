9e3f1a19d2ecbf82773ba04e059a016e007e8c85
Bui Duong <>
2025-04-05 11:22:17 +0700

    fix login

---
+++ app/DTO/GetUserDTO.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DTO;
+
+class GetUserDTO
+{
+    public function __construct(
+        public array $user
+    ) {}
+
+    public static function fromUser($user, $fields = ['id', 'name', 'email'])
+    {
+        $userData = [];
+        foreach ($fields as $field) {
+            if ($field === 'roles') {
+                $user->load('roles');
+                $userData['roles'] = $user->roles->pluck('name')->toArray();
+            } elseif (isset($user->$field)) {
+                $userData[$field] = $user->$field;
+            }
+        }
+
+        return new self(
+            user: $userData
+        );
+    }
+
+    public function toArray(): array
+    {
+        return $this->user; 
+    }
+}
